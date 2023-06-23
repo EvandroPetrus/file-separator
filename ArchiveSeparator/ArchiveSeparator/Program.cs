3e1a04fde0ed61ee629c45ecd7dd9d2cd438b6e6
Evandro Petrus <105548027+EvandroPetrus@users.noreply.github.com>
2023-06-23 16:57:46 -0300

    Add files via upload

---
+++ ArchiveSeparator/ArchiveSeparator/Program.cs
@@ -0,0 +1,24 @@
+﻿using ArchiveSeparator.Services;
+using System;
+using System.IO;
+
+class FileSeparator
+{
+    public static void Main()
+    {
+        //Localização dos arquivos a serem movidos
+        string inputDirectory = "";
+
+        //Localização à qual os arquivos serão movidos
+        string outputDirectory = "";
+
+        //Número de arquivos a serem movidos por pasta
+        int filesPerFolder = 600000;
+
+        ArchiveSeparatorService.SeparateFiles(inputDirectory, outputDirectory, filesPerFolder);
+
+        Console.WriteLine("Separação de arquivos concluída com sucesso.");
+        Console.ReadKey();
+    }
+  
+}
---
+++ ArchiveSeparator/ArchiveSeparator/Services/ArchiveSeparatorService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchiveSeparator.Services
+{
+    public class ArchiveSeparatorService
+    {
+        public static void SeparateFiles(string inputDirectory, string outputDirectory, int filesPerFolder)
+        {
+            try
+            {
+                #region Validação de existência de diretórios
+                if (!Directory.Exists(inputDirectory))
+                {
+                    Console.WriteLine("Source directory does not exist.");
+                    return;
+                }
+
+                if (!Directory.Exists(outputDirectory))
+                {
+                    Directory.CreateDirectory(outputDirectory);
+                }
+                #endregion
+
+                string[] files = Directory.GetFiles(inputDirectory);
+                int fileCount = files.Length;
+                int folderCount = (int)Math.Ceiling((double)fileCount / filesPerFolder);
+
+                for (int i = 0; i < folderCount; i++)
+                {
+                    string folderPath = Path.Combine(outputDirectory, $"Pasta{i + 1}");
+                    Directory.CreateDirectory(folderPath);
+
+                    for (int j = i * filesPerFolder; j < Math.Min((i + 1) * filesPerFolder, fileCount); j++)
+                    {
+                        string filePath = files[j];
+                        string fileName = Path.GetFileName(filePath);
+                        string destinationPath = Path.Combine(folderPath, fileName);
+                        File.Move(filePath, destinationPath);
+                    }
+                }
+            }
+
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine($"Há uma referência nula. {ex.Message}");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu o seguinte erro: {ex.Message}");
+            }
+        }
+    }
+}
